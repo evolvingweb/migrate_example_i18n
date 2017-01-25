0ae00b75996051867f6e2df0210d6daedffc26e9
Jigar Mehta <jigarius@gmail.com>
2017-01-25 14:18:50 -0500

    Implement migration source for node.article

---
+++ src/Plugin/migrate/source/D7_Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\c11n_migrate_i18n\Plugin\migrate\source;
+
+use \Drupal\migrate\Row;
+use \Drupal\node\Plugin\migrate\source\d7\Node;
+
+/**
+ * Drupal 7 node (article) source from database.
+ *
+ * @MigrateSource(
+ *   id = "d7_article"
+ * )
+ */
+class D7_Article extends Node {
+
+}
