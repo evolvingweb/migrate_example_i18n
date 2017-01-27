85cd08997667593b6fe9f74d99041ee03b0ffc5e
Jigar Mehta <jigarius@gmail.com>
2017-01-27 15:58:11 -0500

    Add migration definition for node.story

---
+++ src/Plugin/migrate/source/D6_Story.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\c11n_migrate_i18n\Plugin\migrate\source;
+
+use \Drupal\migrate\Row;
+use \Drupal\node\Plugin\migrate\source\d6\Node;
+
+/**
+ * Drupal 6 node (story) source from database.
+ *
+ * @MigrateSource(
+ *   id = "d6_story"
+ * )
+ */
+class D6_Story extends Node {
+
+}
