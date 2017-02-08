57e485d96878dc64e7da5f83fbd46a6141aeb078
Dave Vasilevsky <dave@vasilevsky.ca>
2017-02-07 21:56:58 -0500

    Fix missing use statement

--- src/Plugin/migrate/source/Dog.php
+++ src/Plugin/migrate/source/Dog.php
@@ -3,6 +3,7 @@
 namespace Drupal\c11n_migrate_i18n\Plugin\migrate\source;
 
 use \Drupal\Core\Database\Query\SelectInterface;
+use Drupal\migrate\Row;
 use \Drupal\node\Plugin\migrate\source\d7\Node;
 
 /**
